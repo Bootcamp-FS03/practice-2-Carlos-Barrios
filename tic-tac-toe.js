1c31d2d54a3efceb919dd46cc6c2304ad394281f
carlosBarriosCarvajal <Carlos.Barrios@jala.university>
2023-12-30 15:58:54 -0500

    feat: tic tac toe game 3x3

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -12,40 +12,88 @@
 
 // DESIRABLE
 // 6. ADD THE ABILITY TO SELECT THE TABLE DIMENSION 3X3, 4X4, 5X5, ETC
+
 let isGameActive = false;
+let player1Name;
+let player2Name;
+let currentPlayer;
 
 const playerForm = document.getElementById('playerForm');
 const gameBoard = document.getElementById('gameBoard');
+const gameContainer = document.getElementById('gameContainer');
+const turnInfo = document.getElementById('turnInfo');
 
 function initializeGameBoard() {
-    // console.log(isGameActive);
-    for (let i = 0; i < 3; i++) {
-        const row = gameBoard.insertRow();
-        for (let j = 0; j < 3; j++) {
-            const cell = row.insertCell();
-            cell.textContent = '';
-        }
-    }
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.textContent = '';
+        cell.addEventListener('click', handleCellClick);
+    });
 }
 
 document.getElementById('playerForm').addEventListener('submit', function (event) {
     event.preventDefault();
 
-    const player1Name = document.getElementById('player1').value;
-    const player2Name = document.getElementById('player2').value;
+    player1Name = document.getElementById('player1').value;
+    player2Name = document.getElementById('player2').value;
 
     if (player1Name && player2Name && player1Name !== player2Name) {
         playerForm.style.display = 'none';
-        gameBoard.innerHTML = '';
+        gameContainer.style.display = 'block';
         isGameActive = true;
         initializeGameBoard();
 
-        const startingPlayer = Math.random() < 0.5 ? player1Name : player2Name;
+        currentPlayer = Math.random() < 0.5 ? player1Name : player2Name;
+        turnInfo.innerText = `${currentPlayer} turn(${currentPlayer === player1Name ? 'X' : 'O'})`;      
 
-        gameBoard.style.display = 'table';
     } else if (!player1Name || !player2Name) {
         document.getElementById('errorMessage').innerText = 'Enter names for both players';
     } else {
         document.getElementById('errorMessage').innerText = 'Player names should be different';
     }
 });
+
+function handleCellClick(event) {
+    if (isGameActive && event.target.textContent === '') {
+        const selectedCell = event.target;
+        selectedCell.textContent = currentPlayer === player1Name ? 'X' : 'O';
+
+        selectedCell.classList.remove('x-cell', 'o-cell');
+
+        selectedCell.classList.add(currentPlayer === player1Name ? 'x-cell' : 'o-cell');
+
+        if (checkWinner()) {
+            console.log(`¡${currentPlayer} won!`);
+            isGameActive = false;
+        } else if (checkTie()) {
+            console.log('¡It´s a tie!');
+            isGameActive = false;
+        } else {
+            currentPlayer = currentPlayer === player1Name ? player2Name : player1Name;
+            turnInfo.innerText = `${currentPlayer} turn (${currentPlayer === player1Name ? 'X' : 'O'})`;
+        }
+    }
+}
+
+function checkWinner() {
+    const cells = document.querySelectorAll('.cell');
+    const winPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Filas
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columnas
+        [0, 4, 8], [2, 4, 6]             // Diagonales
+    ];
+
+    return winPatterns.some(pattern => {
+        const [a, b, c] = pattern;
+        return (
+            cells[a].textContent !== '' &&
+            cells[a].textContent === cells[b].textContent &&
+            cells[b].textContent === cells[c].textContent
+        );
+    });
+}
+
+function checkTie() {
+    const cells = document.querySelectorAll('.cell');
+    return Array.from(cells).every(cell => cell.textContent !== '');
+}
